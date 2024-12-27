da124df891adbaa2dfb0f4dbdf26033c6783921f
Ankush-accio <ankushrohilla2304@gmail.com>
2024-12-27 12:48:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,13 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+  let price = document.querySelectorAll(".prices")
+	let total = price.map((item)=>{
+		total += item
+	})
+	let row = document.createElement("tr")
+	row.innerText = total
+	appendChild(row)
 };
 
 getSumBtn.addEventListener("click", getSum);
